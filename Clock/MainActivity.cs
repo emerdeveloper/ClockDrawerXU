b0ae20a54d2f927099e6ddcadb441e6479183e98
Emerson Javid Gonzalez Morales <1388310@SOAM.TCS.com>
2017-08-30 15:31:30 -0500

    project initial provided by Xamarin university to work implement drawer navigation with DrawerLayout

---
+++ Clock/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+
+namespace Clock
+{
+	[Activity(Label = "Clock", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Android.Support.V7.App.AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+			base.SetSupportActionBar(toolbar);
+			SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu_white_24dp);
+			SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+			Navigate(new TimeFragment());
+		}
+
+		void Navigate(Android.Support.V4.App.Fragment fragment)
+		{
+			var transaction = base.SupportFragmentManager.BeginTransaction();
+			transaction.Replace(Resource.Id.contentFrame, fragment);
+			transaction.Commit();
+		}
+	}
+}
