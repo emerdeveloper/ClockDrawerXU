4336f7a36f84f7f66506ed4892deed49d9ae1cb6
Emerson Javid Gonzalez Morales <1388310@SOAM.TCS.com>
2017-08-31 11:41:34 -0500

    Finished

--- Clock/MainActivity.cs
+++ Clock/MainActivity.cs
@@ -17,7 +17,38 @@ namespace Clock
 			SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu_white_24dp);
 			SupportActionBar.SetDisplayHomeAsUpEnabled(true);
 
+			var menu = FindViewById<Android.Support.Design.Widget.NavigationView>(Resource.Id.navigationView);
+			menu.NavigationItemSelected += OnMenuItemSelected;
+
 			Navigate(new TimeFragment());
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+				case Android.Resource.Id.Home:
+				var drawerLayout = FindViewById<Android.Support.V4.Widget.DrawerLayout>(Resource.Id.drawerLayout);
+				drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+				break;
+			}
+
+			return true;
+		}
+
+		void OnMenuItemSelected(object sender, Android.Support.Design.Widget.NavigationView.NavigationItemSelectedEventArgs e)
+		{
+			switch (e.MenuItem.ItemId)
+			{
+				case Resource.Id.timeMenuItem:      Navigate(new TimeFragment()      ); break;
+				case Resource.Id.stopwatchMenuItem: Navigate(new StopwatchFragment() ); break;
+				case Resource.Id.aboutMenuItem:     Navigate(new AboutFragment()     ); break;
+			}
+
+			e.MenuItem.SetChecked(true);
+
+			var drawerLayout = FindViewById<Android.Support.V4.Widget.DrawerLayout>(Resource.Id.drawerLayout);
+			drawerLayout.CloseDrawer(Android.Support.V4.View.GravityCompat.Start);
 		}
 
 		void Navigate(Android.Support.V4.App.Fragment fragment)
